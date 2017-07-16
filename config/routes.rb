c34f4efe95cbd9116493929fae8adb80e28c061d
Preet Dha <manpreetdha+github@gmail.com>
2017-07-16 13:28:34 -0700

    reorder routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-
+  root 'articles#index'
   resources :articles
 
-  root 'articles#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
