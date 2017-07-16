fa40d9ef7fbc39389a23e22dbe17d9bb93093b89
Preet Dha <email": null>
2017-07-16 12:45:51 -0700

    Make a form partial and add to various pages; seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+20.times do
+  Article.create(title: Faker::HarryPotter.quote, text: Faker::HitchhikersGuideToTheGalaxy.quote)
+end
